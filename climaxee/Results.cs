677e34107595bcfb1b515f05711d8324dc889e35
MariaManu <emanuelebrumrossi0@gmail.com>
2024-04-17 09:50:50 -0300

    iniciamos a descrição da classe e frame

---
+++ climaxee/Results.cs
@@ -0,0 +1,19 @@
+namespace climaxee;
+public class Results
+{
+    public int temp{get;set;}
+    public string description {get;set;}
+    public string eity {get;set;}
+    public double rain {get;set;}
+    public int humidity {get;set;}
+    public double sunrise  {get;set;}
+    public double sunset {get;set;}
+    public double wind_speedy {get;set;}
+    public double wind_direction {get;set;}
+    public string  moon_phase {get;set;}
+    public string condition_code{get;set;}
+    public string img_id {get;set;}
+    public string currently {get;set;}
+    
+
+}
