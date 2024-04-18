af1050f85a8fc064a8e82e292eba34f82ba61f7e
MariaManu <emanuelebrumrossi0@gmail.com>
2024-04-18 11:47:55 -0300

    terminamos o app

--- climaxee/MainPage.xaml.cs
+++ climaxee/MainPage.xaml.cs
@@ -3,12 +3,42 @@
 public partial class MainPage : ContentPage
 {
 	int count = 0;
-
+     Results resultado;
 	public MainPage()
 	{
 		InitializeComponent();
+		TestaLayout();
+		PreencherTela();
 	}
+    void TestaLayout()
+	{
+     resultado = new Results();
+	resultado.temp=23;
+	resultado.description="tempo frio";
+	resultado.rain=10;
+	resultado.city="Apucarana,PR";
+	resultado.humidity=38;
+	resultado.sunrise="06:20";
+	resultado.sunset="18:2";
+	resultado.wind_speedy=3;
+	resultado.wind_direction=395;
+    resultado.moon_phase="nova";
+	}
+	void PreencherTela()
+	{
+       labelTemperatura.Text = resultado.temp.ToString();
+       labelDescrição.Text = resultado.description;
+	   labelCidade.Text = resultado.city;
+	   labelChuva.Text = resultado.rain.ToString();
+	   labelUmidade.Text = resultado.humidity.ToString();
+	   labelAmanhecer.Text = resultado.sunrise;
+       labelAnoitecer.Text = resultado.sunset;
+	   labelForça.Text = resultado.wind_speedy.ToString();
+	   labelDireção.Text = resultado.wind_direction.ToString();
+	   labelFasedalua.Text = resultado.moon_phase;
+	   
 
-	
+	}
 }
+
 
--- climaxee/Results.cs
+++ climaxee/Results.cs
@@ -3,11 +3,11 @@ public class Results
 {
     public int temp{get;set;}
     public string description {get;set;}
-    public string eity {get;set;}
+    public string city {get;set;}
     public double rain {get;set;}
     public int humidity {get;set;}
-    public double sunrise  {get;set;}
-    public double sunset {get;set;}
+    public string sunrise  {get;set;}
+    public string sunset {get;set;}
     public double wind_speedy {get;set;}
     public double wind_direction {get;set;}
     public string  moon_phase {get;set;}
